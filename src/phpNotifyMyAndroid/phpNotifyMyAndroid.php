c370642a7d4a6a71d241a1d4bf49b5765bebb52e
Dennis Koot <koter84@gmail.com>
2013-11-01 12:36:37 +0100

    change verify() to work with multiple api-keys which are comma-separated

--- src/phpNotifyMyAndroid/phpNotifyMyAndroid.php
+++ src/phpNotifyMyAndroid/phpNotifyMyAndroid.php
@@ -4,262 +4,271 @@ namespace phpNotifyMyAndroid;
 
 class phpNotifyMyAndroid
 {
+	/**
+	* @const LIB_ERROR_TYPE can be exception or error
+	*/
+	const LIB_ERROR_TYPE = 'error';
 
-    /**
-     * @const LIB_ERROR_TYPE can be exception or error
-     */
-    const LIB_ERROR_TYPE = 'error';
+	/**
+	* @const holds the api key verify url
+	*/
+	const LIB_NMA_VERIFY = 'https://www.notifymyandroid.com/publicapi/verify';
 
-    /**
-     * @const holds the api key verify url
-     */
-    const LIB_NMA_VERIFY = 'https://www.notifymyandroid.com/publicapi/verify';
+	/**
+	* @const holds the notify url
+	*/
+	const LIB_NMA_NOTIFY = 'https://www.notifymyandroid.com/publicapi/notify';
 
-    /**
-     * @const holds the notify url
-     */
-    const LIB_NMA_NOTIFY = 'https://www.notifymyandroid.com/publicapi/notify';
+	/**
+	* toggles on debugging
+	*
+	* @var bool
+	*/
+	public $debug = false;
+	public $apiCallsRemaining = false;
+	public $apiLimitReset = false;
+	public $lastStatus = false;
 
-    /**
-     * toggles on debugging
-     *
-     * @var bool
-     */
-    public $debug = false;
+	/**
+	* @var bool|string
+	*/
+	protected $apiKey = false;
+	protected $devKey = false;
 
-    public $apiCallsRemaining = false;
+	/**
+	* @var array
+	*/
+	protected $error_codes = array(
+		200 => 'Notification submitted.',
+		400 => 'The data supplied is in the wrong format, invalid length or null.',
+		401 => 'None of the API keys provided were valid.',
+		402 => 'Maximum number of API calls per hour exceeded.',
+		500 => 'Internal server error. Please contact our support if the problem persists.'
+	);
 
-    public $apiLimitReset = false;
+	/**
+	* @param array $options
+	*/
+	function __construct($options = array())
+	{
+		if (!isset($options['apikey']))
+			return $this->error('You must supply a API Key');
+		else
+			$this->apiKey = $options['apikey'];
 
-    public $lastStatus = false;
-    /**
-     * @var bool|string
-     */
-    protected $apiKey = false;
+		if (isset($options['developerkey']))
+			$this->devKey = $options['developerkey'];
 
-    /**
-     * @var bool|string
-     */
-    protected $devKey = false;
+		if (isset($options['debug']))
+			$this->debug = true;
 
+		return true; // this shuts my ide up
+	}
 
-    protected $error_codes
-        = array(
-            200     => 'Notification submitted.',
-            400     => 'The data supplied is in the wrong format, invalid length or null.',
-            401     => 'None of the API keys provided were valid.',
-            402     => 'Maximum number of API calls per hour exceeded.',
-            500     => 'Internal server error. Please contact our support if the problem persists.'
-        );
+	/**
+	* @param bool $key [optional] if not set the one used on construct is used
+	*
+	* @return bool|mixed|SimpleXMLElement|string
+	*/
+	public function verify($key = false)
+	{
+		$options = array();
 
-    /**
-     * @param array $options
-     */
-    function __construct($options = array())
-    {
-        if (!isset($options['apikey'])) {
-            return $this->error('You must supply a API Key');
-        } else {
-            $this->apiKey = $options['apikey'];
-        }
+		if ($key !== false)
+			$options['apikey'] = $key;
+		else
+			$options['apikey'] = $this->apiKey;
+		
+		if ($this->devKey)
+			$options['developerkey'] = $this->devKey;
 
-        if (isset($options['developerkey'])) {
-            $this->devKey = $options['developerkey'];
-        }
+		// check multiple api-keys
+		if(strpos($options['apikey'], ","))
+		{
+			$keys = explode(',', $options['apikey']);
+			foreach($keys as $api)
+			{
+				$options['apikey'] = $api;
+				if(!$this->makeApiCall(self::LIB_NMA_VERIFY, $options))
+					return $this->makeApiCall(self::LIB_NMA_VERIFY, $options);
+			}
+			return true;
+		}
+		else
+		{
+			return $this->makeApiCall(self::LIB_NMA_VERIFY, $options);
+		}
+	}
 
-        if (isset($options['debug'])) {
-            $this->debug = true;
-        }
+	/**
+	* @param string $application
+	* @param string $event
+	* @param string $description
+	* @param int    $priority
+	* @param bool   $apiKeys
+	* @param array  $options
+	*
+	* @return bool|mixed|SimpleXMLElement|string
+	*/
+	public function notify($application = '', $event = '', $description = '', $priority = 0, $apiKeys = false, $options = array())
+	{
+		if (empty($application) || empty($event) || empty($description))
+			return $this->error('you must supply a application name, event and long desc');
 
-        return true; // this shuts my ide up
+		// place here so other parameter settings can override this
+		$post = array();
 
-    }
+		// notify options present? This can be: url or content-type for now.
+		if(count($options) > 0)
+		{
+			foreach($options as $k=>$v)
+				$post[$k] = $v;
+		}
 
+		$post['application']    = substr($application, 0, 256);
+		$post['event']          = substr($event, 0, 1000);
+		$post['description']    = substr($description, 0, 10000);
+		$post['priority']       = $priority;
 
-    /**
-     * @param bool $key [optional] if not set the one used on construct is used
-     *
-     * @return bool|mixed|SimpleXMLElement|string
-     */
-    public function verify($key = false)
-    {
+		if ($this->devKey)
+			$post['developerkey'] = $this->devKey;
 
-        $options = array();
+		if ($apiKeys !== false)
+			$post['apikey'] = $apiKeys;
+		else
+			$post['apikey'] = $this->apiKey;
 
-        if ($key !== false) {
-            $options['apikey'] = $key;
-        } else {
-            $options['apikey'] = $this->apiKey;
-        }
+		return $this->makeApiCall(self::LIB_NMA_NOTIFY, $post, 'POST');
+	}
 
+	/**
+	* @param        $url
+	* @param null   $params
+	* @param string $verb
+	* @param string $format
+	*
+	* @return bool|mixed|SimpleXMLElement|string
+	* @throws Exception
+	*/
+	protected function makeApiCall($url, $params = null, $verb = 'GET', $format = 'xml')
+	{
+		$cparams = array(
+			'http' => array(
+				'method'        => $verb,
+				'ignore_errors' => true
+			)
+		);
+		if ($params !== null && !empty($params))
+		{
+			$params = http_build_query($params, '', '&');
+			if ($verb == 'POST')
+			{
+				$cparams["http"]['header'] = 'Content-Type: application/x-www-form-urlencoded';
+				$cparams['http']['content'] = $params;
+			}
+			else
+			{
+				$url .= '?' . $params;
+			}
+		}
+		else
+		{
+			return $this->error(
+				'this api requires all calls to have params' . $this->debug ? ', you provided: ' . var_dump($params)
+					: ''
+			);
+		}
 
-        if ($this->devKey) {
-            $options['developerkey'] = $this->devKey;
-        }
+		$context = stream_context_create($cparams);
+		$fp = fopen($url, 'rb', false, $context);
+		if (!$fp)
+		{
+			$res = false;
+		}
+		else
+		{
+			if ($this->debug)
+			{
+				$meta = stream_get_meta_data($fp);
+				$this->error('var dump of http headers' . var_dump($meta['wrapper_data']));
+			}
 
-        return $this->makeApiCall(self::LIB_NMA_VERIFY, $options);
-    }
+			$res = stream_get_contents($fp);
+		}
 
-    /**
-     * @param string $application
-     * @param string $event
-     * @param string $description
-     * @param int    $priority
-     * @param bool   $apiKeys
-     * @param array  $options
-     *
-     * @return bool|mixed|SimpleXMLElement|string
-     */
-    public function notify($application = '', $event = '', $description = '', $priority = 0, $apiKeys = false, $options = array())
-    {
-        if (empty($application) || empty($event) || empty($description)) {
-            return $this->error('you must supply a application name, event and long desc');
-        }
+		if ($res === false)
+			return $this->error("$verb $url failed: $php_errormsg");
 
-        // place here so other parameter settings can override this
-        $post = array();
-        
-        // notify options present? This can be: url or content-type for now.
-        if(count($options) > 0) {
-            foreach($options as $k=>$v) {
-                $post[$k] = $v;
-            }
-        }
+		switch ($format)
+		{
+			case 'json':
+				return $this->error('this api does not support json');
+			/*
+			* uncomment the below if json is added later
+			* $r = json_decode($res);
+			if ($r === null) {
+				return $this->error("failed to decode $res as json");
+			}
+			return $r;*/
 
-       $post['application']    = substr($application, 0, 256);
-       $post['event']          = substr($event, 0, 1000);
-       $post['description']    = substr($description, 0, 10000);
-       $post['priority']       = $priority;
+			case 'xml':
+				$r = simplexml_load_string($res);
+				if ($r === null)
+					return $this->error("failed to decode $res as xml");
+				return $this->process_xml_return($r);
+		}
+		return $res;
+	}
 
-        if ($this->devKey) {
-            $post['developerkey'] = $this->devKey;
-        }
+	/**
+	* @param     $message
+	* @param int $type
+	*
+	* @return bool
+	* @throws Exception
+	*/
+	private function error($message, $type = E_USER_NOTICE)
+	{
+		if (self::LIB_ERROR_TYPE == 'error')
+		{
+			trigger_error($message, $type);
+			return false;
+		}
+		else
+		{
+			throw new Exception($message, $type);
+		}
+	}
 
-        if ($apiKeys !== false) {
-            $post['apikey'] = $apiKeys;
-        } else {
-            $post['apikey'] = $this->apiKey;
-        }
+	/**
+	* @param SimpleXMLElement $obj
+	*
+	* @return bool
+	*/
+	private function process_xml_return(\SimpleXMLElement $obj)
+	{
+		if (isset($obj->success))
+		{
+			$this->lastStatus = $obj->success["@attributes"]['code'];
 
-        return $this->makeApiCall(self::LIB_NMA_NOTIFY, $post, 'POST');
-    }
+			$this->apiCallsRemaining = $obj->success["@attributes"]['remaining'];
+			$this->apiLimitReset = $obj->success["@attributes"]['resettimer'];
+			return true;
+		}
+		elseif (isset($obj->error))
+		{
+			if (isset($obj->error["@attributes"]))
+			{
+				$this->lastStatus = $obj->error["@attributes"]['code'];
 
-
-    /**
-     * @param        $url
-     * @param null   $params
-     * @param string $verb
-     * @param string $format
-     *
-     * @return bool|mixed|SimpleXMLElement|string
-     * @throws Exception
-     */
-    protected function makeApiCall($url, $params = null, $verb = 'GET', $format = 'xml')
-    {
-        $cparams = array(
-            'http' => array(
-                'method'        => $verb,
-                'ignore_errors' => true
-            )
-        );
-        if ($params !== null && !empty($params)) {
-            $params = http_build_query($params, '', '&');
-            if ($verb == 'POST') {
-                $cparams["http"]['header'] = 'Content-Type: application/x-www-form-urlencoded';
-                $cparams['http']['content'] = $params;
-            } else {
-                $url .= '?' . $params;
-            }
-        } else {
-            return $this->error(
-                'this api requires all calls to have params' . $this->debug ? ', you provided: ' . var_dump($params)
-                    : ''
-            );
-        }
-
-        $context = stream_context_create($cparams);
-        $fp = fopen($url, 'rb', false, $context);
-        if (!$fp) {
-            $res = false;
-        } else {
-
-            if ($this->debug) {
-                $meta = stream_get_meta_data($fp);
-                $this->error('var dump of http headers' . var_dump($meta['wrapper_data']));
-            }
-
-            $res = stream_get_contents($fp);
-        }
-
-        if ($res === false) {
-            return $this->error("$verb $url failed: $php_errormsg");
-        }
-
-        switch ($format) {
-            case 'json':
-                return $this->error('this api does not support json');
-            /*
-            * uncomment the below if json is added later
-            * $r = json_decode($res);
-           if ($r === null) {
-               return $this->error("failed to decode $res as json");
-           }
-           return $r;*/
-
-            case 'xml':
-                $r = simplexml_load_string($res);
-                if ($r === null) {
-                    return $this->error("failed to decode $res as xml");
-                }
-                return $this->process_xml_return($r);
-        }
-        return $res;
-    }
-
-    /**
-     * @param     $message
-     * @param int $type
-     *
-     * @return bool
-     * @throws Exception
-     */
-    private function error($message, $type = E_USER_NOTICE)
-    {
-        if (self::LIB_ERROR_TYPE == 'error') {
-            trigger_error($message, $type);
-            return false;
-        } else {
-            throw new Exception($message, $type);
-        }
-    }
-
-    /**
-     * @param SimpleXMLElement $obj
-     *
-     * @return bool
-     */
-    private function process_xml_return(\SimpleXMLElement $obj)
-    {
-
-        if (isset($obj->success)) {
-            $this->lastStatus = $obj->success["@attributes"]['code'];
-
-            $this->apiCallsRemaining = $obj->success["@attributes"]['remaining'];
-            $this->apiLimitReset = $obj->success["@attributes"]['resettimer'];
-            return true;
-        } elseif (isset($obj->error)) {
-            if (isset($obj->error["@attributes"])) {
-                $this->lastStatus = $obj->error["@attributes"]['code'];
-
-                if (isset($obj->error["@attributes"]['resettimer'])) {
-                    $this->apiLimitReset = $obj->error["@attributes"]['resettimer'];
-                }
-
-            }
-            return $this->error($obj->error);
-        } else {
-            return $this->error("unkown error");
-        }
-    }
-
+				if (isset($obj->error["@attributes"]['resettimer']))
+					$this->apiLimitReset = $obj->error["@attributes"]['resettimer'];
+			}
+			return $this->error($obj->error);
+		}
+		else
+		{
+			return $this->error("unkown error");
+		}
+	}
 }
